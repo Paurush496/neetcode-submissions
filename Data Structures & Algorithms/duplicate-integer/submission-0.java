457dcdd4deb106f5d793517ec85fc5d3ae3f7351
Paurush496 <paurushchoudhary496@gmail.com>
2026-04-22 18:57:40 +0530

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,6 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> numSet = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        return nums.length != numSet.size();
+    }
+}
