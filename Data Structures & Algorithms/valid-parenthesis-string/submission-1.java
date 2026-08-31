9ec17de6c8c722811c2168b8ef3e5891fa2f4d80
Paurush496 <paurushchoudhary496@gmail.com>
2026-08-31 21:52:04 +0530

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        ArrayDeque<Integer> leftPara = new ArrayDeque<>();
+        ArrayDeque<Integer> star = new ArrayDeque<>();
+        // int leftCount, startCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(')
+                leftPara.push(i);
+                // leftCount++;
+            else if (ch == '*')
+                star.push(i);
+                // startCount++;
+            else if (!leftPara.isEmpty())
+                leftPara.pop();
+            else if (!star.isEmpty())
+                star.pop();
+            else return false;
+        }
+        while(!leftPara.isEmpty() && !star.isEmpty()) {
+            if (star.peekFirst() > leftPara.peekFirst()) {
+                star.pop();
+                leftPara.pop();
+            } else return false;
+        }
+        return leftPara.isEmpty();
+    }
+}
